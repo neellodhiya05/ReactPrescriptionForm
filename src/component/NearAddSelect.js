a513a30c361112e2ee16010ad4e2e82389fba5e6
neellodhiya05 <65756209+neellodhiya05@users.noreply.github.com>
2021-05-15 17:30:31 +0530

    Near addition default vaule changed

--- src/component/NearAddSelect.js
+++ src/component/NearAddSelect.js
@@ -4,84 +4,36 @@ export class NearAddSelect extends Component{
     render(){
         return(
             <div>
-                <select defaultValue={"default"} >
-                    <option value="-4.00">-4.00</option>
-                  
-                    <option value="-3.75">-3.75</option>
-                  
-                    <option value="-3.50">-3.50</option>
-                  
-                    <option value="-3.25">-3.25</option>
-                  
-                    <option value="-3.00">-3.00</option>
-                  
-                    <option value="-2.75">-2.75</option>
-                  
-                    <option value="-2.50">-2.50</option>
-                  
-                    <option value="-2.25">-2.25</option>
-                  
-                    <option value="-2.00">-2.00</option>
-                  
-                    <option value="-1.75">-1.75</option>
-                  
-                    <option value="-1.50">-1.50</option>
-                  
-                    <option value="-1.25">-1.25</option>
-                  
-                    <option value="-1.00">-1.00</option>
-                  
-                    <option value="-0.75">-0.75</option>
-                  
-                    <option value="-0.50">-0.50</option>
-                  
-                    <option value="-0.25">-0.25</option>
-                  
-                    <option value="default" >+/-</option>
-                  
-                    <option value="0.00">0.00</option>
-                  
-                    <option value="INFINITY">∞</option>
-                  
-                    <option value="PLANO">PLANO</option>
-                  
-                    <option value="BALANCE">BALANCE</option>
-                  
-                    <option value="DS">DS</option>
-                  
-                    <option value="+0.25">+0.25</option>
-                  
-                    <option value="+0.50">+0.50</option>
-                  
-                    <option value="+0.75">+0.75</option>
-                  
-                    <option value="+1.00">+1.00</option>
-                  
-                    <option value="+1.25">+1.25</option>
-                  
-                    <option value="+1.50">+1.50</option>
-                  
-                    <option value="+1.75">+1.75</option>
-                  
-                    <option value="+2.00">+2.00</option>
-                  
-                    <option value="+2.25">+2.25</option>
-                  
-                    <option value="+2.50">+2.50</option>
-                  
-                    <option value="+2.75">+2.75</option>
-                  
-                    <option value="+3.00">+3.00</option>
-                  
-                    <option value="+3.25">+3.25</option>
-                  
-                    <option value="+3.50">+3.50</option>
-                  
-                    <option value="+3.75">+3.75</option>
-                  
-                    <option value="+4.00">+4.00</option>
-                  
-                  </select>
+                <select defaultValue={"63"}>
+  <option value="63" >--</option>
+
+  <option value="+0.50">+0.50</option>
+
+  <option value="+0.75">+0.75</option>
+
+  <option value="+1.00">+1.00</option>
+
+  <option value="+1.25">+1.25</option>
+
+  <option value="+1.50">+1.50</option>
+
+  <option value="+1.75">+1.75</option>
+
+  <option value="+2.00">+2.00</option>
+
+  <option value="+2.25">+2.25</option>
+
+  <option value="+2.50">+2.50</option>
+
+  <option value="+2.75">+2.75</option>
+
+  <option value="+3.00">+3.00</option>
+
+  <option value="+3.25">+3.25</option>
+
+  <option value="+3.50">+3.50</option>
+
+</select>
                   
             </div>
             

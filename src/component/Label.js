87b91f3a370035b9f44ba0ee6c215547ff0aed18
neellodhiya05 <65756209+neellodhiya05@users.noreply.github.com>
2021-05-25 17:41:16 +0530

    some changes

--- src/component/Label.js
+++ src/component/Label.js
@@ -1,21 +1,25 @@
+import { PausePresentationTwoTone } from "@material-ui/icons";
 import Axios from "axios";
-import React,{Component, useState} from "react";
+import React,{ useState} from "react";
 
 export function Label (){
-    const url =""
+    const url ="/api/formulas/create"
     const [data,SetData] =useState({
         name: ""
     })
     function submit(e){
-        alert('submit called')
         e.preventDefault();
         Axios.post(url,{
-            name: data.name
+            name: parseInt(data.name),
         })
-         .then(res => {
-              console.log(res.data)
-         })
-    }
+        .then(response => { 
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error.response)
+        });
+    }   
+    
     function handle (e){
         const newdata={  ...data}      
         newdata[e.target.id] =e.target.value
@@ -53,7 +57,6 @@ export function Label (){
 
 
         )
-    
-}
+        }
 
 
